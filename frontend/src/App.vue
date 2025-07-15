5aeded49516465223fd43f8d990f0f85e908633a
Liam <cariou.liam@orange.fr>
2025-07-15 17:32:59 +0200

    Ajout d'une condition dans la navbar pour l'affichage d'un lien

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,11 +14,12 @@
           to="/conferences"
           class="hover:text-blue-400 font-medium px-3 py-1 rounded transition"
         >Conférences</router-link>
-           <router-link
-          v-if="isLogged"
-          to="/mes-conferences"
-          class="hover:text-blue-400 font-medium px-3 py-1 rounded transition"
-        >Mes conférences</router-link>
+       <router-link
+  v-if="isLogged && isSpeaker"
+  to="/mes-conferences"
+  class="hover:text-blue-400 font-medium px-3 py-1 rounded transition"
+>Mes conférences</router-link>
+
         <router-link
           v-if="isLogged"
           to="/me"
@@ -59,6 +60,17 @@ function logout() {
   localStorage.clear()
   window.location = "/home"
 }
+const isSpeaker = computed(() => {
+  if (!localStorage.getItem('user')) return false
+  try {
+    const user = JSON.parse(localStorage.getItem('user'))
+    const roles = JSON.parse(user.Roles)
+    return roles.includes("conferencier")
+  } catch {
+    return false
+  }
+})
+
 
 onMounted(() => {
   const params = new URLSearchParams(window.location.search)
